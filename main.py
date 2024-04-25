ae000d98ea568693e185eca2d7fb84b7df0b6237
keroxxkrxx <167641806+keroxxkrxx@users.noreply.github.com>
2024-04-25 23:35:01 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import telebot
 import requests
+import traceback
 
-bot = telebot.TeleBot('-')
+bot = telebot.TeleBot('6389998596:AAHtSHLjpzl0hOPH5198ZuaggwPPcmwUkAk')
+admin_id = 
 
 start_txt = 'Привет! Это бот прогноза погоды.\nНапиши любой символ чтобы\nузнать погоду в Новосибирске на данный момент.'
 
@@ -23,33 +25,36 @@ def start(message):
 
 @bot.message_handler(content_types=['text'])
 def weather(message):
-    city = "Новосибирск"
-    url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&lang=ru&appid=79d1ca96933b0328e1c7e3e7a26cb347'
-    responce = requests.get(url)
-    if responce.status_code == 200:
-        if message.text == 'Информация о проекте':
-            bot.send_message(message.from_user.id, 'Мы сделали проект про бота, который может\nприсылать погоду в реальном времени.', reply_markup=keyboard2)
-        elif message.text == 'Информация о создателях проекта':
-            bot.send_message(message.from_user.id, 'Создатели проекта: Кирилл Кошелев, Виктор Пономарев\n'
-                                                   'Ученики 7 "Г" класса ', reply_markup=keyboard2)
+    try:
+        city = "Новосибирск"
+        url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&lang=ru&appid=79d1ca96933b0328e1c7e3e7a26cb347'
+        responce = requests.get(url)
+        if responce.status_code == 200:
+            if message.text == 'Информация о проекте':
+                bot.send_message(message.from_user.id, 'Мы сделали проект про бота, который может\nприсылать погоду в реальном времени.', reply_markup=keyboard2)
+            elif message.text == 'Информация о создателях проекта':
+                bot.send_message(message.from_user.id, 'Создатели проекта: Кирилл Кошелев, Виктор Пономарев\n'
+                                                       'Ученики 7 "Г" класса ', reply_markup=keyboard2)
+            else:
+                url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&lang=ru&appid=79d1ca96933b0328e1c7e3e7a26cb347'
+                weather_data = requests.get(url).json()
+                temperature = round(weather_data['main']['temp'])
+                temperature_feels = round(weather_data['main']['feels_like'])
+                humidity = round(weather_data['main']['humidity'])
+                clouds = round(weather_data['clouds']['all'])
+                w_now = 'Сейчас в городе ' + city + ' ' + str(temperature) + ' °C'
+                w_feels = 'Ощущается как ' + str(temperature_feels) + ' °C'
+                w_hum = 'Влажность: '+ str(humidity) +'%'
+                w_clouds = 'Облачность: ' + str(clouds) + '%'
+                bot.send_message(message.from_user.id, w_now, reply_markup = keyboard)
+                bot.send_message(message.from_user.id, w_feels, reply_markup = keyboard)
+                bot.send_message(message.from_user.id, w_hum, reply_markup = keyboard)
+                bot.send_message(message.from_user.id, w_clouds, reply_markup = keyboard)
         else:
-            url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&lang=ru&appid=79d1ca96933b0328e1c7e3e7a26cb347'
-            weather_data = requests.get(url).json()
-            temperature = round(weather_data['main']['temp'])
-            temperature_feels = round(weather_data['main']['feels_like'])
-            humidity = round(weather_data['main']['humidity'])
-            clouds = round(weather_data['clouds']['all'])
-            w_now = 'Сейчас в городе ' + city + ' ' + str(temperature) + ' °C'
-            w_feels = 'Ощущается как ' + str(temperature_feels) + ' °C'
-            w_hum = 'Влажность: '+ str(humidity) +'%'
-            w_clouds = 'Облачность: ' + str(clouds) + '%'
-            bot.send_message(message.from_user.id, w_now, reply_markup = keyboard)
-            bot.send_message(message.from_user.id, w_feels, reply_markup = keyboard)
-            bot.send_message(message.from_user.id, w_hum, reply_markup = keyboard)
-            bot.send_message(message.from_user.id, w_clouds, reply_markup = keyboard)
-    else:
-        bot.send_message(message.from_user.id, 'Бот не обработал запрос, попробуйте позже.' ,reply_markup=keyboard)
-
+            bot.send_message(message.from_user.id, 'Бот не обработал запрос, попробуйте позже.' ,reply_markup=keyboard)
+            bot.send_message(admin_id, f'status_code: {responce.status_code}')
+    except:
+        bot.send_message(admin_id, f'Error in weather: {traceback.format_exc()}')
 
 bot.polling(none_stop=True, interval=0)
 
